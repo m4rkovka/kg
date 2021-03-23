dca02dcbb67ad007ab0161019e71b15d8ba2a1b0
m4rkovka <markov.workfrl@gmail.com>
2021-03-23 19:16:45 +0300

    change ellipsoid for dima

--- lab03/ellipsoid.cpp
+++ lab03/ellipsoid.cpp
@@ -6,8 +6,8 @@
 
 Ellipsoid::Ellipsoid() {
     a = 4;
-    b = 4;
-    c = 4;
+    b = 3;
+    c = 2;
     step = 0.1;
     lamp = new Lamp(70, 0, 0, 100);
 
@@ -44,10 +44,10 @@ void Ellipsoid::set_step(double s) {
 void Ellipsoid::create() {
     std::vector<QVector4D> prevPoints{};
     QVector4D firstIter{0, 0, static_cast<float>(c * cos(0)), 1};
-    QVector4D lastIter{0, 0, 0, 1};
+    QVector4D lastIter{0, 0, static_cast<float>(c * cos(M_PI)), 1};
     bool connectToOnePoint = true;
     for (double theta = step / 2.; theta < M_PI; theta += step / 2.) {
-        if (connectToOnePoint) {
+        if (connectToOnePoint && theta + step < M_PI) {
             QVector4D prevVertex;
             QVector4D firstVertex;
             for (double phi = 0.; phi < 2 * M_PI; phi += step) {
@@ -144,113 +144,21 @@ void Ellipsoid::create() {
                     polygons.push_back(toPushBack);
                     newPrevPoints.push_back(firstVertex);
                     prevPoints = newPrevPoints;
+                    if (theta + step / 2. > M_PI) {
+                        connectToOnePoint = true;
+                    }
                 }
             }
         }
     }
-    prevPoints.clear();
-    connectToOnePoint = true;
-    for (double r = step; r < a; r += step) {
-        QVector4D prevVertex, firstVertex;
-        if (connectToOnePoint) {
-            for (double t = 0; t < 2 * M_PI; t += step) {
-                if (t == 0.) {
-                    firstVertex = {
-                        static_cast<float>(r * cos(t)),
-                        static_cast<float>(r * sin(t)),
-                        0,
-                        1
-                    };
-                    prevVertex = firstVertex;
-                    prevPoints.push_back(prevVertex);
-                    continue;
-                }
-                std::vector<QVector4D> toPushBack;
-                QVector4D newVertex;
-                newVertex = {
-                    static_cast<float>(r * cos(t)),
-                    static_cast<float>(r * sin(t)),
-                    0,
-                    1
-                };
-                toPushBack = {
-                    lastIter,
-                    prevVertex,
-                    newVertex
-                };
-                polygons.push_back(toPushBack);
-                prevVertex = newVertex;
-                prevPoints.push_back(prevVertex);
-                if (t + step >= 2 * M_PI) {
-                    toPushBack = {
-                        lastIter,
-                        prevVertex,
-                        firstVertex
-                    };
-                    polygons.push_back(toPushBack);
-                    prevPoints.push_back(firstVertex);
-                    connectToOnePoint = false;
-                }
-
-            }
-        } else {
-            QVector4D prevVertex;
-            QVector4D firstVertex;
-            std::vector<QVector4D> newPrevPoints{};
-            size_t cnt = 0;
-            for (double phi = 0; phi < 2 * M_PI; phi += step, cnt++) {
-                if (phi == 0.) {
-                    firstVertex = {
-                        static_cast<float>(a * sin(theta) * cos(phi)),
-                        static_cast<float>(b * sin(theta) * sin(phi)),
-                        static_cast<float>(c * cos(theta)),
-                        1
-                    };
-                    prevVertex = firstVertex;
-                    newPrevPoints.push_back(prevVertex);
-                    continue;
-                }
-                std::vector<QVector4D> toPushBack;
-                QVector4D newVertex;
-                newVertex = {
-                    static_cast<float>(a * sin(theta) * cos(phi)),
-                    static_cast<float>(b * sin(theta) * sin(phi)),
-                    static_cast<float>(c * cos(theta)),
-                    1
-                };
-                toPushBack = {
-                    prevPoints[cnt - 1],
-                    prevVertex,
-                    newVertex
-                };
-                polygons.push_back(toPushBack);
-                toPushBack = {
-                    prevPoints[cnt - 1],
-                    newVertex,
-                    prevPoints[cnt]
-                };
-                polygons.push_back(toPushBack);
-                prevVertex = newVertex;
-                newPrevPoints.push_back(prevVertex);
-                if (phi + step > 2 * M_PI) {
-                    cnt++;
-                    toPushBack = {
-                        prevPoints[cnt - 1],
-                        prevVertex,
-                        firstVertex
-                    };
-                    polygons.push_back(toPushBack);
-                    toPushBack = {
-                        prevPoints[cnt - 1],
-                        firstVertex,
-                        prevPoints[cnt]
-                    };
-                    polygons.push_back(toPushBack);
-                    newPrevPoints.push_back(firstVertex);
-                    prevPoints = newPrevPoints;
-                }
-            }
-        }
+    std::vector<QVector4D> toPushBack;
+    for (size_t i = 0; i < prevPoints.size() - 1; i++) {
+        toPushBack = {
+            prevPoints[i + 1],
+            prevPoints[i],
+            lastIter
+        };
+        polygons.push_back(toPushBack);
     }
 }
 
